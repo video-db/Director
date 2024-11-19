a58fdaab9c1f66eae196640d8e7a9813204e7343
Ankit raj <113342181+ankit-v2-3@users.noreply.github.com>
2024-11-19 21:41:00 +0530

    feat: add format response

--- backend/director/agents/composio.py
+++ backend/director/agents/composio.py
@@ -1,10 +1,19 @@
 import os
 import logging
+import json
 
 from director.agents.base import BaseAgent, AgentResponse, AgentStatus
-from director.core.session import Session
+from director.core.session import (
+    Session,
+    ContextMessage,
+    RoleTypes,
+    TextContent,
+    MsgStatus,
+)
 
 from director.tools.composio_tool import composio_tool
+from director.llm.openai import OpenAI
+from director.llm.base import LLMResponseStatus
 
 logger = logging.getLogger(__name__)
 
@@ -24,8 +33,8 @@ class ComposioAgent(BaseAgent):
     def __init__(self, session: Session, **kwargs):
         self.agent_name = "composio"
         self.description = f'The Composio agent is used to run tasks related to apps like {os.getenv("COMPOSIO_APPS")} '
-
         self.parameters = COMPOSIO_PARAMETERS
+        self.llm = OpenAI()
         super().__init__(session=session, **kwargs)
 
     def run(self, task: str, *args, **kwargs) -> AgentResponse:
@@ -38,10 +47,33 @@ class ComposioAgent(BaseAgent):
         try:
             self.output_message.actions.append("Running task..")
             self.output_message.push_update()
+
+            text_content = TextContent(
+                agent_name=self.agent_name,
+                status=MsgStatus.progress,
+            )
+            self.output_message.content.append(text_content)
+            self.output_message.push_update()
+
             composio_response = composio_tool(task=task)
+            llm_prompt = (
+                "Format the following reponse into text.\n"
+                "Give the output which can be directly send to use \n"
+                "Don't add any extra text \n"
+                f"{json.dumps(composio_response)}"
+            )
+            composio_response = ContextMessage(content=llm_prompt, role=RoleTypes.user)
+            llm_response = self.llm.chat_completions([composio_response.to_llm_msg()])
+            if llm_response.status == LLMResponseStatus.ERROR:
+                raise Exception(f"LLM Failed with error {llm_response.content}")
+
+            text_content.text = llm_response.content
+            text_content.status = MsgStatus.success
+            text_content.status_message = "Here is the response from Composio"
 
         except Exception as e:
             logger.exception(f"Error in {self.agent_name}")
+            text_content.status = MsgStatus.error
             self.output_message.publish()
             error_message = f"Agent failed with error {e}"
             return AgentResponse(status=AgentStatus.ERROR, message=error_message)
