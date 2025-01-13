02f68788830ebc2662ba92a1fe5eb6b47994022d
0xrohitgarg <rohit@spext.co>
2025-01-13 13:55:52 +0530

    rever logs

--- backend/director/utils/asyncio.py
+++ backend/director/utils/asyncio.py
@@ -1,11 +1,8 @@
 import asyncio
 
-
 def is_event_loop_running():
     try:
-        loop = asyncio.get_running_loop()
-        print("EVENT LOOP CHECK : Event loop is running", loop)
+        asyncio.get_running_loop()
         return True
-    except RuntimeError as e:
-        print("EVENT LOOP CHECK : Event loop is not running", e)
+    except RuntimeError:
         return False
