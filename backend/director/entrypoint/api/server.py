723931a0b7f4a043190500376aaa3c4867d7c38b
nitishvarmaus <nitishvarma001@gmail.com>
2025-02-04 12:47:29 +0530

    Refactor Devzery integration to conditionally import and initialize based on environment variables

--- backend/director/entrypoint/api/server.py
+++ backend/director/entrypoint/api/server.py
@@ -3,8 +3,6 @@ import os
 from dotenv import load_dotenv
 from director.entrypoint.api import create_app
 
-from devzery import Devzery
-
 load_dotenv()
 
 
@@ -82,12 +80,14 @@ configs = dict(local=LocalAppConfig, production=ProductionAppConfig)
 # By default, the server is configured to run in development mode. To run in production mode, set the `SERVER_ENV` environment variable to `production`.
 app = create_app(app_config=configs[os.getenv("SERVER_ENV", "local")])
 
-devzery = Devzery(
-    api_key=os.getenv("DEVZERY_API_KEY"),
-    source_name=os.getenv("DEVZERY_SOURCE_NAME")
-)
+if os.getenv("DEVZERY_API_KEY") and os.getenv("DEVZERY_SOURCE_NAME"):
+    from devzery import Devzery
+    devzery = Devzery(
+        api_key=os.getenv("DEVZERY_API_KEY"),
+        source_name=os.getenv("DEVZERY_SOURCE_NAME")
+    )
 
-devzery.flask_middleware(app)
+    devzery.flask_middleware(app)
 
 if __name__ == "__main__":
     app.run(
